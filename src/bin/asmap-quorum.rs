cc4bbd302534166997b1e0e60e65cc2e23f78a8a
randymcmillan <randymcmillan@protonmail.com>
2026-08-11 14:23:14 -0400

    src/bin/asmap-quorum.rs:intermediate

--- src/bin/asmap-quorum.rs
+++ src/bin/asmap-quorum.rs
@@ -203,3 +203,9 @@ async fn main() -> Result<(), Box<dyn Error>> {
         }
     }
 }
+fn main() {
+    if let Err(err) = bitcoin_asmap_quorum::run() {
+        eprintln!("{err:#}");
+        std::process::exit(1);
+    }
+}
