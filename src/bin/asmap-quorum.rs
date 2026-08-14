120db8c34bb118cc836ff61d93f4d8d0222cada4
randymcmillan <randymcmillan@protonmail.com>
2026-08-14 07:22:55 -0400

    src/bin/asmap-quorum.rs:use CARGO_BIN_NAME

--- src/bin/asmap-quorum.rs
+++ src/bin/asmap-quorum.rs
@@ -3,7 +3,7 @@ fn main() {
     let _ = env_logger::Builder::from_env(env)
         .format_timestamp_millis()
         .try_init();
-    if let Err(err) = bitcoin_asmap_quorum::run() {
+    if let Err(err) = bitcoin_asmap_quorum::run_with_binary_name(env!("CARGO_BIN_NAME")) {
         eprintln!("{err:#}");
         std::process::exit(1);
     }
